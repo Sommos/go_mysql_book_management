191ca0588e20786f5c1964e923e543d77113a645
Sommos <Sommosgaming@gmail.com>
2023-07-13 12:10:42 +0100

    added controller functions GetBook, GetBookByID, CreateBook, DeleteBook, UpdateBook

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 )
 
-func ParseBody(r *http.Request, x interface{}) error {
+func ParseBody(r *http.Request, x interface{}) {
 	if body, err := ioutil.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
